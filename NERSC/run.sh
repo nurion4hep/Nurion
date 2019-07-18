1be6867cb3f6df47aae7b9be5df384d062b258a7
ico1036 <jiwoong.kim@cern.sh>
2019-07-18 19:03:45 +0900

    preprocess_v2

--- NERSC/run.sh
+++ NERSC/run.sh
@@ -2,20 +2,29 @@
 
 StartTime=$(date +%s)
 
-##### -- DATA preprocessing + Train 
-
+### --train.py
 #python train.py --nb-epochs 20 --nb-train-events 412416 --batch-size 512 ./train.h5 ./val.h5 3ch-CNN  # Full set
 #python train.py --nb-epochs 10 --nb-train-events 100000 --nb-test-events 10000 --batch-size 512 ./train.h5 ./val.h5 3ch-CNN
-python train.py --nb-epochs 10 --nb-train-events 20000 --nb-test-events 1000 --batch-size 512 ./train.h5 ./val.h5 3ch-CNN
+#python train.py --nb-epochs 10 --nb-train-events 20000 --nb-test-events 1000 --batch-size 512 ./train.h5 ./val.h5 3ch-CNN
+
+
+### ---preprocess.py
+#python preprocess.py --nb-train-events 100000 --nb-test-events 10000 ./train.h5 ./val.h5 3ch-CNN
+
+
+### ---preprocess_TFD.py
+#python preprocess_TFD.py --nb-train-events 100000 --nb-test-events 10000 ./train.h5 ./val.h5 3ch-CNN
 
 
 
+### --train_only.py
+python train_only.py --nb-epochs 10 --nb-train-events 20000 --nb-test-events 1000 --batch-size 512 ./Preprocessed_Train.h5 ./Preprocessed_Val.h5 3ch-CNN
 
-#####  ---1. DATA preprocessing(Run this before "2.Train Only" )
-#python preprocess.py --nb-train-events 20000 --nb-test-events 1000 ./train.h5 ./val.h5 3ch-CNN
 
-##### --- 2. Train Only( Run this afeter "1.DATA preprocessing" )
-#python train_only.py --nb-epochs 10 --nb-train-events 20000 --nb-test-events 1000 --batch-size 512 ./Preprocessed_Train.h5 ./Preprocessed_Val.h5 3ch-CNN
+### --train_only_v2.py HDF5Matrix test
+#python train_only_v2.py --nb-epochs 10 --nb-train-events 20000 --nb-test-events 1000 --batch-size 512 ./Preprocessed_Train.h5 ./Preprocessed_Val.h5 3ch-CNN
+
+python test.py
 
 EndTime=$(date +%s)
 
--- NERSC/train_only.py
+++ NERSC/train_only.py
@@ -2,200 +2,201 @@ import h5py
 import argparse
 import numpy as np
 
+from keras.utils.io_utils import HDF5Matrix
 def get_parser():
-    parser = argparse.ArgumentParser(
-        description='Run SUSY RPV training',
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter
-    )
-    parser.add_argument('--nb-epochs', action='store', type=int, default=50,
-                        help='Number of epochs to train for.')
+	parser = argparse.ArgumentParser(
+		description='Run SUSY RPV training',
+		formatter_class=argparse.ArgumentDefaultsHelpFormatter
+	)
+	parser.add_argument('--nb-epochs', action='store', type=int, default=50,
+						help='Number of epochs to train for.')
 
-    parser.add_argument('--nb-train-events', action='store', type=int, default=1000,
-                        help='Number of events to train on.')
-    
-    parser.add_argument('--nb-test-events', action='store', type=int, default=999999999,
-                        help='Number of events to test on.')
+	parser.add_argument('--nb-train-events', action='store', type=int, default=1000,
+						help='Number of events to train on.')
+	
+	parser.add_argument('--nb-test-events', action='store', type=int, default=999999999,
+						help='Number of events to test on.')
 
-    parser.add_argument('--batch-size', action='store', type=int, default=256,
-                        help='batch size per update')
+	parser.add_argument('--batch-size', action='store', type=int, default=256,
+						help='batch size per update')
 
-    parser.add_argument('train_data', action='store', type=str,
-                        help='path to HDF5 file to train on')
+	parser.add_argument('train_data', action='store', type=str,
+						help='path to HDF5 file to train on')
 
-    parser.add_argument('val_data', action='store', type=str,
-                        help='path to HDF5 file to validate on')
+	parser.add_argument('val_data', action='store', type=str,
+						help='path to HDF5 file to validate on')
 
-    parser.add_argument('model', action='store', type=str,
-                        help='one of: "3ch-CNN", "CNN", "FCN", "BDT"')
+	parser.add_argument('model', action='store', type=str,
+						help='one of: "3ch-CNN", "CNN", "FCN", "BDT"')
 
-    return parser
+	return parser
 
 if __name__ == '__main__':
-    parser = get_parser()
-    args = parser.parse_args()
+	parser = get_parser()
+	args = parser.parse_args()
 
-    # -- check that model makes sense:
-    if args.model not in ['3ch-CNN', 'CNN', 'FCN', 'BDT']:
-        raise ValueError("The model type needs to be one of '3ch-CNN', 'CNN', 'FCN', 'BDT'.")
+	# -- check that model makes sense:
+	if args.model not in ['3ch-CNN', 'CNN', 'FCN', 'BDT']:
+		raise ValueError("The model type needs to be one of '3ch-CNN', 'CNN', 'FCN', 'BDT'.")
 
-    # -- load data:
+	# -- load data:
 	print("############## LOAD DATA ###################")
-    data = h5py.File(args.train_data)
-    images = data['images'][:args.nb_train_events]
-    labels = data['labels'][:args.nb_train_events]
-    weights = data['weights'][:args.nb_train_events]
-    val = h5py.File(args.val_data)
-    images_val = val['images_val'][:args.nb_test_events]
-    labels_val = val['labels_val'][:args.nb_test_events]
-    weights_val = val['weights_val'][:args.nb_test_events]  
+	data 	= h5py.File(args.train_data)
+	images 	= data['all_events']['images'][:args.nb_train_events]
+	labels 	= data['all_events']['labels'][:args.nb_train_events]
+	weights	= data['all_events']['weights'][:args.nb_train_events]
+	val 		= h5py.File(args.val_data)
+	images_val 	= val['all_events']['images_val'][:args.nb_test_events]
+	labels_val 	= val['all_events']['labels_val'][:args.nb_test_events]
+	weights_val = val['all_events']['weights_val'][:args.nb_test_events]  
 
-    # -- output file names
-    model_weights = 'model_weights_log_' + args.model + '.h5'
-    predictions_file = 'prediction_nn_log_' + args.model + '.pyc'
+	
+	
+	# -- output file names
+	model_weights = 'model_weights_log_' + args.model + '.h5'
+	predictions_file = 'prediction_nn_log_' + args.model + '.pyc'
 
-    if args.model == 'BDT':
-        from sklearn.ensemble import GradientBoostingClassifier
-        from sklearn.model_selection import GridSearchCV
-        base_clf = GradientBoostingClassifier(verbose=2)
-        parameters = {
-            'n_estimators': [50, 100],
-            'max_depth':[3, 5]
-        }
-        clf = GridSearchCV(base_clf, parameters, n_jobs=4, fit_params={'sample_weight': weights})
-        clf.fit(images.reshape(images.shape[0], -1), labels)
-        yhat = clf.predict_proba(images_val.reshape(images_val.shape[0], -1))
-        np.save(predictions_file, yhat)
+	if args.model == 'BDT':
+		from sklearn.ensemble import GradientBoostingClassifier
+		from sklearn.model_selection import GridSearchCV
+		base_clf = GradientBoostingClassifier(verbose=2)
+		parameters = {
+			'n_estimators': [50, 100],
+			'max_depth':[3, 5]
+		}
+		clf = GridSearchCV(base_clf, parameters, n_jobs=4, fit_params={'sample_weight': weights})
+		clf.fit(images.reshape(images.shape[0], -1), labels)
+		yhat = clf.predict_proba(images_val.reshape(images_val.shape[0], -1))
+		np.save(predictions_file, yhat)
 
-    elif 'CNN' in args.model:
-        from keras.layers import (Input, Conv2D, LeakyReLU,
-            BatchNormalization, MaxPooling2D, Dropout, Dense, Flatten)
-        from keras.models import Model
-        from keras.callbacks import EarlyStopping, ModelCheckpoint
+	elif 'CNN' in args.model:
+		from keras.layers import (Input, Conv2D, LeakyReLU,
+			BatchNormalization, MaxPooling2D, Dropout, Dense, Flatten)
+		from keras.models import Model
+		from keras.callbacks import EarlyStopping, ModelCheckpoint
 
+		x = Input(shape=(images.shape[1], images.shape[2], images.shape[3]))
+		h = Conv2D(64, kernel_size=(3, 3), activation='relu', strides=1, padding='same')(x)
+		h = BatchNormalization()(h)																																					 
+		h = MaxPooling2D(pool_size=(2, 2))(h)																																		   
+		h = Dropout(0.5)(h)				  
+		h = Conv2D(128, kernel_size=(3, 3), activation='relu', strides=2, padding='same')(h)
+		h = BatchNormalization()(h)																																					 
+		h = MaxPooling2D(pool_size=(2, 2))(h)																																		   
+		h = Dropout(0.5)(h)				  
+		h = Conv2D(256, kernel_size=(3, 3), activation='relu', strides=1, padding='same')(h)
+		h = BatchNormalization()(h)																																					 
+		h = MaxPooling2D(pool_size=(2, 2))(h)
+		h = Conv2D(256, kernel_size=(3, 3), activation='relu', strides=2, padding='same')(h)
+		h = BatchNormalization()(h)		  
+		h = Flatten()(h)
+		h = Dense(512, activation='relu')(h)
+		h = Dropout(0.5)(h)
+		y = Dense(1, activation='sigmoid')(h)
+		model = Model(
+			inputs = x,
+			outputs = y
+		)
+		model.summary()
+		model.compile(
+			optimizer='adam',
+			loss='binary_crossentropy',
+			metrics=['accuracy']
+		)
+		try:
+			model.load_weights(model_weights)
+			print 'Weights loaded from ' + model_weights
+		except IOError:
+			print 'No pre-trained weights found'
+		try:
+			model.fit(images, labels,
+					  batch_size=args.batch_size,
+					  sample_weight=weights,
+					  epochs=args.nb_epochs,
+					  verbose=1,
+					  callbacks = [
+						EarlyStopping(verbose=True, patience=20, monitor='val_loss'),
+						ModelCheckpoint(model_weights,
+							monitor='val_loss', verbose=True, save_best_only=True)
+					  ],
+					  validation_data=(images_val, labels_val, weights_val)
+			)
+		except KeyboardInterrupt:
+			print 'Training finished early'
 
-        x = Input(shape=(images.shape[1], images.shape[2], images.shape[3]))
-        h = Conv2D(64, kernel_size=(3, 3), activation='relu', strides=1, padding='same')(x)
-        h = BatchNormalization()(h)                                                                                                                                                     
-        h = MaxPooling2D(pool_size=(2, 2))(h)                                                                                                                                           
-        h = Dropout(0.5)(h)                  
-        h = Conv2D(128, kernel_size=(3, 3), activation='relu', strides=2, padding='same')(h)
-        h = BatchNormalization()(h)                                                                                                                                                     
-        h = MaxPooling2D(pool_size=(2, 2))(h)                                                                                                                                           
-        h = Dropout(0.5)(h)                  
-        h = Conv2D(256, kernel_size=(3, 3), activation='relu', strides=1, padding='same')(h)
-        h = BatchNormalization()(h)                                                                                                                                                     
-        h = MaxPooling2D(pool_size=(2, 2))(h)
-        h = Conv2D(256, kernel_size=(3, 3), activation='relu', strides=2, padding='same')(h)
-        h = BatchNormalization()(h)          
-        h = Flatten()(h)
-        h = Dense(512, activation='relu')(h)
-        h = Dropout(0.5)(h)
-        y = Dense(1, activation='sigmoid')(h)
-        model = Model(
-            inputs = x,
-            outputs = y
-        )
-        model.summary()
-        model.compile(
-            optimizer='adam',
-            loss='binary_crossentropy',
-            metrics=['accuracy']
-        )
-        try:
-            model.load_weights(model_weights)
-            print 'Weights loaded from ' + model_weights
-        except IOError:
-            print 'No pre-trained weights found'
-        try:
-            model.fit(images, labels,
-                      batch_size=args.batch_size,
-                      sample_weight=weights,
-                      epochs=args.nb_epochs,
-                      verbose=1,
-                      callbacks = [
-                        EarlyStopping(verbose=True, patience=20, monitor='val_loss'),
-                        ModelCheckpoint(model_weights,
-                            monitor='val_loss', verbose=True, save_best_only=True)
-                      ],
-                      validation_data=(images_val, labels_val, weights_val)
-            )
-        except KeyboardInterrupt:
-            print 'Training finished early'
+		model.load_weights(model_weights)
+		yhat = model.predict(images_val, verbose=1, batch_size=args.batch_size)
+	   # score = model.evaluate(images_val, labels_val, sample_weight=weights_val, verbose=1)
+	   # print 'Validation loss:', score[0]
+	   # print 'Validation accuracy:', score[1]
+		np.save(predictions_file, yhat)
 
-        model.load_weights(model_weights)
-        yhat = model.predict(images_val, verbose=1, batch_size=args.batch_size)
-       # score = model.evaluate(images_val, labels_val, sample_weight=weights_val, verbose=1)
-       # print 'Validation loss:', score[0]
-       # print 'Validation accuracy:', score[1]
-        np.save(predictions_file, yhat)
+	elif args.model == 'FCN':
+		from keras.layers import (Input, Conv2D, LeakyReLU,
+			BatchNormalization, MaxPooling2D, Dropout, Dense, Flatten)
+		from keras.models import Model
+		from keras.callbacks import EarlyStopping, ModelCheckpoint
 
-    elif args.model == 'FCN':
-        from keras.layers import (Input, Conv2D, LeakyReLU,
-            BatchNormalization, MaxPooling2D, Dropout, Dense, Flatten)
-        from keras.models import Model
-        from keras.callbacks import EarlyStopping, ModelCheckpoint
-
-        x = Input(shape=(images.shape[1] * images.shape[2], ))        
-        #h = Flatten()(x)
-        h = Dense(2048, activation='relu')(x)
-        h = Dropout(0.5)(h)
-        h = Dense(2048, activation='relu')(h)
-        h = Dropout(0.5)(h)
-        h = Dense(2048, activation='relu')(h)
-        h = Dropout(0.5)(h)
-        h = Dense(2048, activation='relu')(h)
-        h = Dropout(0.5)(h)
-        h = Dense(2048, activation='relu')(h)
-        h = Dropout(0.5)(h)
-        h = Dense(2048, activation='relu')(h)
-        h = Dropout(0.5)(h)
-        h = Dense(2048, activation='relu')(h)
-        h = Dropout(0.5)(h)
-        h = Dense(2048, activation='relu')(h)
-        h = Dropout(0.5)(h)
-        y = Dense(1, activation='sigmoid')(h)
-        model = Model(
-            inputs = x,
-            outputs = y
-        )
-        model.summary()
-        model.compile(
-            optimizer='adam',
-            loss='binary_crossentropy',
-            metrics=['accuracy',tf.keras.metrics.FalsePositives(name='false_positives'),tf.keras.metrics.FalseNegatives(name='false_negatives'),tf.keras.metrics.TrueNegatives(name='true_negatives'),tf.keras.metrics.TruePositives(name='true_positives')]
+		x = Input(shape=(images.shape[1] * images.shape[2], ))		
+		#h = Flatten()(x)
+		h = Dense(2048, activation='relu')(x)
+		h = Dropout(0.5)(h)
+		h = Dense(2048, activation='relu')(h)
+		h = Dropout(0.5)(h)
+		h = Dense(2048, activation='relu')(h)
+		h = Dropout(0.5)(h)
+		h = Dense(2048, activation='relu')(h)
+		h = Dropout(0.5)(h)
+		h = Dense(2048, activation='relu')(h)
+		h = Dropout(0.5)(h)
+		h = Dense(2048, activation='relu')(h)
+		h = Dropout(0.5)(h)
+		h = Dense(2048, activation='relu')(h)
+		h = Dropout(0.5)(h)
+		h = Dense(2048, activation='relu')(h)
+		h = Dropout(0.5)(h)
+		y = Dense(1, activation='sigmoid')(h)
+		model = Model(
+			inputs = x,
+			outputs = y
+		)
+		model.summary()
+		model.compile(
+			optimizer='adam',
+			loss='binary_crossentropy',
+			metrics=['accuracy',tf.keras.metrics.FalsePositives(name='false_positives'),tf.keras.metrics.FalseNegatives(name='false_negatives'),tf.keras.metrics.TrueNegatives(name='true_negatives'),tf.keras.metrics.TruePositives(name='true_positives')]
 		)
 
 
 	
 	
-	print("############## FIT DATA ###################")
 
 
-        try:
-            model.load_weights(model_weights)
-            print 'Weights loaded from ' + model_weights
-        except IOError:
-            print 'No pre-trained weights found'
+		try:
+			model.load_weights(model_weights)
+			print 'Weights loaded from ' + model_weights
+		except IOError:
+			print 'No pre-trained weights found'
 
-        try:
-            model.fit(images.reshape(images.shape[0], -1), labels,
-                      batch_size=args.batch_size,
-                      sample_weight=weights,
-                      epochs=args.nb_epochs,
-                      verbose=1,
-                      callbacks = [
-                        EarlyStopping(verbose=True, patience=20, monitor='val_loss'),
-                        ModelCheckpoint(model_weights,
-                            monitor='val_loss', verbose=True, save_best_only=True)
-                      ],
-                      validation_data=(images_val.reshape(images_val.shape[0], -1), labels_val, weights_val)
-            )
-        except KeyboardInterrupt:
-            print 'Training finished early'
+		try:
+			model.fit(images.reshape(images.shape[0], -1), labels,
+					  batch_size=args.batch_size,
+					  sample_weight=weights,
+					  epochs=args.nb_epochs,
+					  verbose=1,
+					  callbacks = [
+						EarlyStopping(verbose=True, patience=20, monitor='val_loss'),
+						ModelCheckpoint(model_weights,
+							monitor='val_loss', verbose=True, save_best_only=True)
+					  ],
+					  validation_data=(images_val.reshape(images_val.shape[0], -1), labels_val, weights_val)
+			)
+		except KeyboardInterrupt:
+			print 'Training finished early'
 
-        model.load_weights(model_weights)
-        yhat = model.predict(images_val.reshape(images_val.shape[0], -1), verbose=1, batch_size=args.batch_size)
-        # score = model.evaluate(images_val, labels_val, sample_weight=weights_val, verbose=1)
-        # print 'Validation loss:', score[0]
-        # print 'Validation accuracy:', score[1]
-        np.save(predictions_file, yhat)
+		model.load_weights(model_weights)
+		yhat = model.predict(images_val.reshape(images_val.shape[0], -1), verbose=1, batch_size=args.batch_size)
+		# score = model.evaluate(images_val, labels_val, sample_weight=weights_val, verbose=1)
+		# print 'Validation loss:', score[0]
+		# print 'Validation accuracy:', score[1]
+		np.save(predictions_file, yhat)
