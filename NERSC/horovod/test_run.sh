8d01b1e85becac35707680cd4c82494c38fcd570
ico1036 <jiwoong@cern.ch>
2019-07-25 17:21:02 +0900

    HEP_CNN_horovod

--- NERSC/horovod/test_run.sh
+++ NERSC/horovod/test_run.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
+                                                                                                                                                                
 #PBS -V
 #PBS -N tf_mnist
 #PBS -q normal
 #PBS -W sandbox=PRIVATE
 #PBS -A etc
 #PBS -l select=4:ncpus=68:mpiprocs=1:ompthreads=68
-#PBS -l walltime=02:00:00
-
+#PBS -l walltime=04:00:00
 export HDF5_USE_FILE_LOCKING='FALSE'
 module load gcc/7.2.0 openmpi/3.1.0 craype-mic-knl tensorflow/1.12.0
 
 cd $PBS_O_WORKDIR
 
 beginTime=$(date +%s%N)
-mpirun python train_only.py --nb-epochs 12 --nb-train-events 100000 --nb-test-events 10000 --batch-size 512 data/Preprocessed_Train.h5 data/Preprocessed_Val.h5 3ch-CNN 
+mpirun python ./keras.py
 
 endTime=$(date +%s%N)
 elapsed=`echo "($endTime - $beginTime) / 1000000" | bc`
