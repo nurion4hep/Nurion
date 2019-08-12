ad219b4e5bdcddd0d1173d1558156769893a0867
ico1036 <jiwoong.kim@cern.sh>
2019-08-12 14:55:08 +0900

    c

--- NERSC/train_only.py
+++ NERSC/train_only.py
@@ -1,20 +1,8 @@
 import h5py
 import argparse
 import numpy as np
-import os
-import errno
-import tensorflow as tf
-import numpy as np
-from tensorflow import keras
-import math
-from tensorflow.keras import layers
-import sys
-sys.modules['keras'] = keras
-import horovod.tensorflow.keras as hvd
-#from mpi4py import MPI
 
-
-
+from keras.utils.io_utils import HDF5Matrix
 def get_parser():
 	parser = argparse.ArgumentParser(
 		description='Run SUSY RPV training',
@@ -50,19 +38,6 @@ if __name__ == '__main__':
 	# -- check that model makes sense:
 	if args.model not in ['3ch-CNN', 'CNN', 'FCN', 'BDT']:
 		raise ValueError("The model type needs to be one of '3ch-CNN', 'CNN', 'FCN', 'BDT'.")
-	
-	### Horovod:  init
-	hvd.init()
-	
-	# Horovod: pin GPU to be used to process local rank (one GPU per process)
-	config = tf.ConfigProto()
-	config.gpu_options.allow_growth = True
-	config.gpu_options.visible_device_list = str(hvd.local_rank())
-	config.intra_op_parallelism_threads = 64
-	config.inter_op_parallelism_threads = 1
-	keras.backend.set_session(tf.Session(config=config))
-
-	print("horovod: hvdsize: ",hvd.size())
 
 	# -- load data:
 	print("############## LOAD DATA ###################")
@@ -95,77 +70,59 @@ if __name__ == '__main__':
 		np.save(predictions_file, yhat)
 
 	elif 'CNN' in args.model:
-		
-			
-		x = layers.Input(shape=(images.shape[1], images.shape[2], images.shape[3]))
-		h = layers.Conv2D(64, kernel_size=(3, 3), activation='relu', strides=1, padding='same')(x)
-		h = layers.BatchNormalization()(h)																																					 
-		h = layers.MaxPooling2D(pool_size=(2, 2))(h)																																		   
-		h = layers.Dropout(0.5)(h)				  
-		h = layers.Conv2D(128, kernel_size=(3, 3), activation='relu', strides=2, padding='same')(h)
-		h = layers.BatchNormalization()(h)																																					 
-		h = layers.MaxPooling2D(pool_size=(2, 2))(h)																																		   
-		h = layers.Dropout(0.5)(h)				  
-		h = layers.Conv2D(256, kernel_size=(3, 3), activation='relu', strides=1, padding='same')(h)
-		h = layers.BatchNormalization()(h)																																					 
-		h = layers.MaxPooling2D(pool_size=(2, 2))(h)
-		h = layers.Conv2D(256, kernel_size=(3, 3), activation='relu', strides=2, padding='same')(h)
-		h = layers.BatchNormalization()(h)		  
-		h = layers.Flatten()(h)
-		h = layers.Dense(512, activation='relu')(h)
-		h = layers.Dropout(0.5)(h)
-		y = layers.Dense(1, activation='sigmoid')(h)
-		model = tf.keras.Model(
+		from keras.layers import (Input, Conv2D, LeakyReLU,
+			BatchNormalization, MaxPooling2D, Dropout, Dense, Flatten)
+		from keras.models import Model
+		from keras.callbacks import EarlyStopping, ModelCheckpoint
+
+		x = Input(shape=(images.shape[1], images.shape[2], images.shape[3]))
+		h = Conv2D(64, kernel_size=(3, 3), activation='relu', strides=1, padding='same')(x)
+		h = BatchNormalization()(h)																																					 
+		h = MaxPooling2D(pool_size=(2, 2))(h)																																		   
+		h = Dropout(0.5)(h)				  
+		h = Conv2D(128, kernel_size=(3, 3), activation='relu', strides=2, padding='same')(h)
+		h = BatchNormalization()(h)																																					 
+		h = MaxPooling2D(pool_size=(2, 2))(h)																																		   
+		h = Dropout(0.5)(h)				  
+		h = Conv2D(256, kernel_size=(3, 3), activation='relu', strides=1, padding='same')(h)
+		h = BatchNormalization()(h)																																					 
+		h = MaxPooling2D(pool_size=(2, 2))(h)
+		h = Conv2D(256, kernel_size=(3, 3), activation='relu', strides=2, padding='same')(h)
+		h = BatchNormalization()(h)		  
+		h = Flatten()(h)
+		h = Dense(512, activation='relu')(h)
+		h = Dropout(0.5)(h)
+		y = Dense(1, activation='sigmoid')(h)
+		model = Model(
 			inputs = x,
 			outputs = y
 		)
 		model.summary()
-		
-		
-
-		# Horovod: Set epoch and distributed optimizer
-		nb_epochs = int(math.ceil(args.nb_epochs / hvd.size()))
-		opt = tf.keras.optimizers.Adam(0.001 * hvd.size())
-		opt = hvd.DistributedOptimizer(opt)
 		model.compile(
-			optimizer=opt,
+			optimizer='adam',
 			loss='binary_crossentropy',
 			metrics=['accuracy']
 		)
-
-		print("###########################")
-		print("hvd rank: ",hvd.rank())
-		print("###########################")
-		
-
-		callbacks=[
-		hvd.callbacks.BroadcastGlobalVariablesCallback(0),
-		tf.keras.callbacks.EarlyStopping(verbose=True, patience=20, monitor='val_loss'),
-		]
-		
-		if hvd.rank()==0:
-			callbacks.append(tf.keras.callbacks.ModelCheckpoint('./checkpoint-{epoch}.h5'))
-			callbacks.append(tf.keras.callbacks.ModelCheckpoint(model_weights,
-        	monitor='val_loss', verbose=True, save_best_only=True)
-        	)
-		
-	
 		try:
 			model.load_weights(model_weights)
-			print('Weights loaded from ' + model_weights)
+			print 'Weights loaded from ' + model_weights
 		except IOError:
-			print('No pre-trained weights found')
+			print 'No pre-trained weights found'
 		try:
 			model.fit(images, labels,
-					  batch_size=args.batch_size / 64,
+					  batch_size=args.batch_size,
 					  sample_weight=weights,
-					  epochs=nb_epochs,
+					  epochs=args.nb_epochs,
 					  verbose=1,
-					  callbacks = callbacks,
+					  callbacks = [
+						EarlyStopping(verbose=True, patience=20, monitor='val_loss'),
+						ModelCheckpoint(model_weights,
+							monitor='val_loss', verbose=True, save_best_only=True)
+					  ],
 					  validation_data=(images_val, labels_val, weights_val)
 			)
 		except KeyboardInterrupt:
-			print('Training finished early')
+			print 'Training finished early'
 
 		model.load_weights(model_weights)
 		yhat = model.predict(images_val, verbose=1, batch_size=args.batch_size)
@@ -174,3 +131,72 @@ if __name__ == '__main__':
 	   # print 'Validation accuracy:', score[1]
 		np.save(predictions_file, yhat)
 
+	elif args.model == 'FCN':
+		from keras.layers import (Input, Conv2D, LeakyReLU,
+			BatchNormalization, MaxPooling2D, Dropout, Dense, Flatten)
+		from keras.models import Model
+		from keras.callbacks import EarlyStopping, ModelCheckpoint
+
+		x = Input(shape=(images.shape[1] * images.shape[2], ))		
+		#h = Flatten()(x)
+		h = Dense(2048, activation='relu')(x)
+		h = Dropout(0.5)(h)
+		h = Dense(2048, activation='relu')(h)
+		h = Dropout(0.5)(h)
+		h = Dense(2048, activation='relu')(h)
+		h = Dropout(0.5)(h)
+		h = Dense(2048, activation='relu')(h)
+		h = Dropout(0.5)(h)
+		h = Dense(2048, activation='relu')(h)
+		h = Dropout(0.5)(h)
+		h = Dense(2048, activation='relu')(h)
+		h = Dropout(0.5)(h)
+		h = Dense(2048, activation='relu')(h)
+		h = Dropout(0.5)(h)
+		h = Dense(2048, activation='relu')(h)
+		h = Dropout(0.5)(h)
+		y = Dense(1, activation='sigmoid')(h)
+		model = Model(
+			inputs = x,
+			outputs = y
+		)
+		model.summary()
+		model.compile(
+			optimizer='adam',
+			loss='binary_crossentropy',
+			metrics=['accuracy',tf.keras.metrics.FalsePositives(name='false_positives'),tf.keras.metrics.FalseNegatives(name='false_negatives'),tf.keras.metrics.TrueNegatives(name='true_negatives'),tf.keras.metrics.TruePositives(name='true_positives')]
+		)
+
+
+	
+	
+
+
+		try:
+			model.load_weights(model_weights)
+			print 'Weights loaded from ' + model_weights
+		except IOError:
+			print 'No pre-trained weights found'
+
+		try:
+			model.fit(images.reshape(images.shape[0], -1), labels,
+					  batch_size=args.batch_size,
+					  sample_weight=weights,
+					  epochs=args.nb_epochs,
+					  verbose=1,
+					  callbacks = [
+						EarlyStopping(verbose=True, patience=20, monitor='val_loss'),
+						ModelCheckpoint(model_weights,
+							monitor='val_loss', verbose=True, save_best_only=True)
+					  ],
+					  validation_data=(images_val.reshape(images_val.shape[0], -1), labels_val, weights_val)
+			)
+		except KeyboardInterrupt:
+			print 'Training finished early'
+
+		model.load_weights(model_weights)
+		yhat = model.predict(images_val.reshape(images_val.shape[0], -1), verbose=1, batch_size=args.batch_size)
+		# score = model.evaluate(images_val, labels_val, sample_weight=weights_val, verbose=1)
+		# print 'Validation loss:', score[0]
+		# print 'Validation accuracy:', score[1]
+		np.save(predictions_file, yhat)
