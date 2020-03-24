defe6adb29ef3d196bf42fe789d89a60100c6288
JiWoongKim <jkimak1124@gmail.com>
2020-03-24 23:27:32 +0900

    hdf5

--- GenSample/simple_view.py
+++
@@ -1,8 +0,0 @@
-import h5py
-import numpy as np
-
-data=h5py.File("test_output.h5")
-
-print("#### all_event branches #####")
-print(list(data["all_events"].keys()))
-
---
+++ hdf5/run_simple.sh
@@ -0,0 +1,5 @@
+
+for i in `seq 1 23`
+do
+python3 simple_view.py /xrootd_user/hknam/xrootd2/sample4Nurion/2018DAS/Delphes/200318/hdf5/RPV_1400/RPV_1400-$i\.h5
+done
