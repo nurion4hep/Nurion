9f9a82342914d36bb50d9d3c10ec3c6e9fd9f6b2
ico1036 <jiwoong.kim@cern.ch>
2020-04-02 11:47:51 +0900

    speedup

--- Analyze_Tool/node_vs_time.py
+++ Analyze_Tool/node_vs_time.py
@@ -1,5 +1,5 @@
 import matplotlib
-#matplotlib.use('Agg')
+matplotlib.use('Agg')
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
@@ -9,18 +9,23 @@ import numpy as np
 #time = [2223.3251526355743, 1451.9124898910522, 1137.9917290210724, 1140.3619375228882, 1166.649931192398,1014.0974791049957,1353.4979276657104]
 
 ## 32K new 100 epoch default
-time = [3694.408791065216, 2829.7513008117676, 2580.9733810424805, 2276.055812358856, 2361.5011258125305,2221.3792428970337,2680.1628584861755]
+#time = [3694.408791065216, 2829.7513008117676, 2580.9733810424805, 2276.055812358856, 2361.5011258125305,2221.3792428970337,2680.1628584861755]
+
+## 8Kfix for speedup
+time=[258567.16841275874,160685.8866262436,85292.47870731354,25507.26446390152,15490.927917003632,8999.204348564148,5223.29126906395,2850.1766772270203,1731.3487522602081,1353.4979276657104]
+
 
 #log scl 32K
 #time = [2117.8648080825806,1450.6409270763397,1160.1696314811707,1022.107349872589,878.6063137054443,840.2194459438324]
 
 #log scl 16K
 #time = [2440.980729341507, 1764.3287065029144, 1551.672635793686, 1397.4543027877808, 1392.6179225444794,1300.065186738968]
-node=[64,128,256,512,1024,2048,4096]
+
+node=[4,8,16,64,128,256,512,1024,2048,4096]
 
 
 
-text = ['64node','128','256','512','1024','2048','4096']
+text = ['4node','8','16','64','128','256','512','1024','2048','4096']
 
 
 title='Training elapsed time'
@@ -33,12 +38,12 @@ axs.plot(node,time,'--bo',color='royalblue',markersize=12)
 
 i=0
 for x,y in zip(node,time):
-	
-	if(i!=3):
-		plt.text(x+7,y-8,text[i],fontsize=18)
+	if i!=4 or i!=5:
+		plt.text(x+9,y-8,text[i],fontsize=15)
+		print(i, text[i])
+	else:
+		plt.text(x-7,y-16,text[i],fontsize=15)
 	i+=1
-plt.text(node[3]+40,time[3]-8,text[3],fontsize=18)
-
 
 axs.set_title(title,fontsize=25)
 axs.set_xlabel('Node',fontsize=25)
@@ -46,6 +51,6 @@ axs.set_ylabel('Time',fontsize=25)
 axs.grid()
 
 plt.tight_layout()
-plt.show()
+#plt.show()
 plt.savefig('node_vs_time.png')
 
--- Analyze_Tool/speedup.py
+++ Analyze_Tool/speedup.py
@@ -5,33 +5,36 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # 16K results
-#time=[36670.725098371506, 9921.49017047882, 2970.891015291214, 1718.982388496399, 1106.7916436195374, 640.1377544403076, 425.4936149120331,303.94]
-#Single_time=36670.725098371506
-#node=[4,16,64,128,256,512,1024,2048]
+time_old=[36670.725098371506, 9921.49017047882, 2970.891015291214, 1718.982388496399, 1106.7916436195374, 640.1377544403076, 425.4936149120331,303.94]
+Single_time_old=36670.725098371506
+node_old=[4,16,64,128,256,512,1024,2048]
 
 # 32K results
-time = [85292.47870731354,25507.26446390152,15490.927917003632,8999.204348564148,5223.29126906395,2680.1628584861755]
-Single_time=85292.47870731354
-node=[16,64,128,256,512,4096]
-#node=[16,64,128,256,512,1024,2048,4096]
+time=[258567.16841275874,160685.8866262436,85292.47870731354,25507.26446390152,15490.927917003632,8999.204348564148,5223.29126906395,2850.1766772270203,1731.3487522602081,1353.4979276657104]
+Single_time= 258567.16841275874
+node=[4,8,16,64,128,256,512,1024,2048,4096]
+
+speedup_old =[]
+for i in time_old:
+	speedup_old.append(4 * Single_time_old / i)
+	#speedup.append(16 * Single_time / i)
 
 speedup =[]
 for i in time:
-	#speedup.append(4 * Single_time / i)
-	speedup.append(16 * Single_time / i)
-print(speedup)
-
+	speedup.append(4 * Single_time / i)
+	#speedup.append(16 * Single_time / i)
 
 plt.rc('xtick',labelsize=15)
 plt.rc('ytick',labelsize=15)
 
 #x = np.linspace(0,2100,21000)
-x = np.linspace(16,4000,21000)
+x = np.linspace(4,5000,20000)
 y = x
 fig,axs = plt.subplots(1,figsize=(10,10))
-axs.plot(node,speedup,'--*',color='midnightblue',markersize=10)
+axs.plot(node,speedup,'--*',color='midnightblue',markersize=10,label="Current speedup(Batch=8 Fixed)")
+axs.plot(node_old,speedup_old,'--o',color='royalblue',markersize=10,alpha=0.6,label="Old speedup(Batch=128 fixed")
 axs.plot(x,y,'--',color='maroon',alpha=0.5)
-
+plt.legend(prop={'size':20})
 
 xmin=10
 xmax=5000
@@ -45,12 +48,12 @@ axs.set_xlim([xmin,xmax])
 axs.set_ylim([ymin,ymax])
 
 #minor_ticks = np.arange(4, 5000, 10)
-minor_ticks = np.arange(16, 5000, 10)
+minor_ticks = np.arange(4, 5000, 10)
 
 #axs.set_xticks([4,5,6,7,10,20,30,100,200,1000,2000,3000,4000])
-axs.set_xticks([16,60,100,200,1000,2000,4000])
+axs.set_xticks([4,16,60,100,200,1000,2000,4000])
 axs.set_xticks(minor_ticks, minor=True)
-axs.set_yticks([16,60,100,200,300,1000,2000,4000])
+axs.set_yticks([4,16,60,100,200,300,1000,2000,4000])
 axs.set_yticks(minor_ticks, minor=True)
 
 
