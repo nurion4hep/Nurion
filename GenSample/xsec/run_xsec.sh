21b02f6e77dce83ec288b8bebb199a1417ddc88a
JiWoongKim <jkimak1124@gmail.com>
2020-08-20 10:07:40 +0900

    xsec

---
+++ GenSample/xsec/run_xsec.sh
@@ -0,0 +1,36 @@
+
+
+
+
+
+
+cmsRun ana.py  inputFiles="/store/mc/RunIIAutumn18MiniAOD/SMS-T1tbs_RPV_mGluino1400_TuneCP2_13TeV-madgraphMLM-pythia8/MINIAODSIM/102X_upgrade2018_realistic_v15-v1/30000/F72D48A6-8077-324A-BEB3-65A93C3CDF66.root" maxEvents=-1 
+
+
+
+
+## RPV
+#echo  "########################### RPV ##############################"
+#cmsRun ana.py  inputFiles="root://cmsxrootd.fnal.gov//store/mc/RunIIAutumn18MiniAOD/SMS-T1tbs_RPV_mGluino1400_TuneCP2_13TeV-madgraphMLM-pythia8/MINIAODSIM/102X_upgrade2018_realistic_v15-v1/30000/F95ACB05-1B54-C84A-9F94-C1684CAFA706.root" maxEvents=-1 2>&1 | tee RPV.log
+#
+#
+## QCD HT700-1000
+#echo  "########################### QCD700 ##############################"
+#cmsRun ana.py  inputFiles="root://cmsxrootd.fnal.gov//store/mc/RunIIAutumn18MiniAOD/QCD_HT700to1000_TuneCP5_13TeV-madgraphMLM-pythia8/MINIAODSIM/102X_upgrade2018_realistic_v15-v1/80000/81D85ACE-7A97-354B-AAB6-4480ABA5E3FE.root" maxEvents=-1 2>&1 | tee QCD700.log
+#
+#
+## QCD HT1000-1500
+#echo  "########################### QCD1000 ##############################"
+#cmsRun ana.py inputFiles="root://cmsxrootd.fnal.gov//store/mc/RunIIAutumn18MiniAOD/QCD_HT1000to1500_TuneCP5_13TeV-madgraphMLM-pythia8/MINIAODSIM/102X_upgrade2018_realistic_v15-v1/80000/FFE5DF41-7F79-6A43-8384-B808623CB69E.root" maxEvents=-1 2>&1 | tee QCD1000.log
+#
+#
+## QCD HT1500-2000
+#echo  "########################### QCD1500 ##############################"
+#cmsRun ana.py inputFiles="root://cmsxrootd.fnal.gov//store/mc/RunIIAutumn18MiniAOD/QCD_HT1500to2000_TuneCP5_13TeV-madgraphMLM-pythia8/MINIAODSIM/102X_upgrade2018_realistic_v15-v1/80000/FECAAFAF-32B5-994C-B500-FAC36BBB8F60.root" maxEvents=-1 2>&1 | tee QCD1500.log
+#
+#
+## QCD HT2000-InF
+#echo  "########################### QCD2000 ##############################"
+#cmsRun ana.py inputFiles="root://cmsxrootd.fnal.gov//store/mc/RunIIAutumn18MiniAOD/QCD_HT2000toInf_TuneCP5_13TeV-madgraphMLM-pythia8/MINIAODSIM/102X_upgrade2018_realistic_v15-v1/910000/E607A2D5-5E66-8848-8057-62D223A84264.root" maxEvents=-1 2>&1 | tee QCD2000.log
+#
+
