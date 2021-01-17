8e3e76d4aaf0b7e4c8c3ab254289634321b8cf1d
ico1036 <jiwoong,kim@cern.ch>
2021-01-18 00:06:36 +0900

    m

--- Analyze_Tool/speedup.py
+++ Analyze_Tool/speedup.py
@@ -1,5 +1,4 @@
 import matplotlib
-matplotlib.use('Agg')
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
@@ -49,54 +48,53 @@ import numpy as np
 # ---------------------------------------------------------------
 
 # 32K 224x224 CP Large Kernel LR=2e-03
-#time = [166.53976332187653 ,101.92912660121918 ,70.34434948921204 ,56.16551579713821]
-#Single_time = time[0]
-#node = [128,256,512,1024]
+#time = [166.53976332187653 ,101.92912660121918 ,70.34434948921204 ,56.16551579713821 ,58.9195232200622] # with 2048
+#time_224 = [166.53976332187653 ,101.92912660121918 ,70.34434948921204 ,56.16551579713821]
+#Single_time_224 = time_224[0]
+#node_224 = [128,256,512,1024]
 
 
 # 8batch 224x224 CP Large Kernel LR=2e-03
-time = [17308.84763431549 ,10376.94586956501 ,5391.960455553873 ,1655.170814064833 ,842.9652311944961 ,438.2046036052704 ,229.2290646457672 ,134.4215600824356]
-Single_time = time[0]
-node=[4,8,16,64,128,256,512,1024]
+time_224 = [17308.84763431549 ,10376.94586956501 ,5391.960455553873 ,1655.170814064833 ,842.9652311944961 ,438.2046036052704 ,229.2290646457672 ,134.4215600824356]
+Single_time_224 = time_224[0]
+node_224=[4,8,16,64,128,256,512,1024]
 
 
-# 32K 224x224 CP Large Kernel LR=2e-03
-#speedup = []
-#for i in time:
-#	speedup.append(128 * Single_time / i )
+# 32K 64x64 DefaultNorm0 LR=8e-03
+#time_64 = [52.64287549972534 ,34.76981614112854 ,26.27677938938141 ,23.179390041828157 ,26.423430993556977] # with 64
+#time_64 = [34.76981614112854 ,26.27677938938141 ,23.179390041828157 ,26.423430993556977] 
+#Single_time_64 = time_64[0]
+#node_64 = [64,128,256,512,1024]
+#node_64 = [128,256,512,1024]
 
-# 8batch 224x224 CP Large Kernel LR=2e-03
-speedup = []
-for i in time:
-	speedup.append(4 * Single_time / i )
-
-
-
-
-
-#speedup_B128 =[]
-#for i in time_B128:
-#	speedup_B128.append(4 * Single_time_B128 / i)
-#	#speedup.append(16 * Single_time / i)
+# 8batch 64x64 DefaultNorm0 LR=8e-03
+time_64 = [4754.470872561137 ,2544.376339018345 ,1367.4141018159928 ,397.29915613889693 ,287.7252774953842 ,125.58801151514054 ,86.47991998195648 ,52.25830784082413]
+Single_time_64 = time_64[0]
+node_64=[4,8,16,64,128,256,512,1024]
 
 
-#speedup_B8 =[]
-#for i in time_B8:
-#	speedup_B8.append(128 * Single_time_B8 / i)
+# strong 224x224 CP Large Kernel LR=2e-03
+#speedup_224 = []
+#for i in time_224:
+#	speedup_224.append(128 * Single_time_224 / i )
+
+# strong 64x64 CP DefaultNorm0  LR=8e-03
+#speedup_64 = []
+#for i in time_64:
+#	speedup_64.append(64 * Single_time_64 / i )
 
 
-#speedup_B8_large =[]
-#for i in time_B8_large:
-#	speedup_B8_large.append(128 * Single_time_B8_large / i)
+# weak 224x224 CP Large Kernel LR=2e-03
+speedup_224 = []
+for i in time_224:
+	speedup_224.append(4 * Single_time_224 / i )
+
+# weak 64x64 CP DefaultNorm0  LR=8e-03
+speedup_64 = []
+for i in time_64:
+	speedup_64.append(4 * Single_time_64 / i )
 
 
-#speedup_large =[]
-#for i in time_large:
-#	speedup_large.append(128 * Single_time_large / i)
-
-#speedup_32K =[]
-#for i in time_32K:
-#	speedup_32K.append(128 * Single_time_32K / i)
 
 
 
@@ -105,56 +103,52 @@ for i in time:
 plt.rc('xtick',labelsize=15)
 plt.rc('ytick',labelsize=15)
 
-x = np.linspace(0,2100,21000)
-#x = np.linspace(128,5000,20000)
+# --strong
+#x = np.linspace(60,5000,20000)
+# --weak
+x = np.linspace(3,5000,20000)
+
+
 y = x
 fig,axs = plt.subplots(1,figsize=(10,10))
-#axs.plot(node_B128,speedup_B128,'--*',color='green',markersize=10,alpha=0.6,label="2019 Fall(Batch=128 fixed)")
-
-#axs.plot(node_B8,speedup_B8,'--*',color='midnightblue',markersize=10,label="64x64(Batch=8 Fixed)")
-#axs.plot(node_B8_large,speedup_B8_large,'--*',color='crimson',markersize=10,label="224x224(Batch=8 Fixed)")
-axs.plot(node,speedup,'--*',color='crimson',markersize=10,label="speedup per epoch")
-
-
-#axs.plot(node_32K,speedup_32K,'--*',color='midnightblue',markersize=10,alpha=0.6,label="64x64 EffectiveBatch=32K")
-#axs.plot(node_large,speedup_large,'--o',color='crimson',markersize=10,alpha=1,label="224x224 EffectiveBatch=32K fixed")
+axs.plot(node_224,speedup_224,'--*',color='crimson',markersize=10,label="224x224")
+axs.plot(node_64,speedup_64,'--*',color='royalblue',markersize=10,label="64x64")
 axs.plot(x,y,'--',color='gray',alpha=0.5)
 plt.legend(prop={'size':15})
 
-xmin=1
+xmin=3
 xmax=1200
-ymin=1
+ymin=3
 ymax=1200
+
 axs.set_yscale('log')
 axs.set_xscale('log')
-axs.set_xlabel('Node',fontsize=20)
+axs.set_xlabel('Number of nodes',fontsize=20)
 axs.set_ylabel('Speedp-up per epoch',fontsize=20)
 axs.set_xlim([xmin,xmax])
 axs.set_ylim([ymin,ymax])
 
-#minor_ticks = np.arange(4, 5000, 10)
-#axs.set_xticks([4,5,6,7,10,20,30,100,200,1000,2000,3000,4000])
-#axs.set_xticks([4,16,60,100,200,1000,2000,4000])
-#axs.set_xticks(minor_ticks, minor=True)
-#axs.set_yticks([4,16,60,100,200,300,1000,2000,4000])
-#axs.set_yticks(minor_ticks, minor=True)
 
-#minor_ticks = np.arange(128, 5000, 10)
-minor_ticks = np.arange(4, 1200, 10)
-#axs.set_xticks([128,200,1000,2000,3000,4000])
-#axs.set_xticks([128,200,400,600,800,1000,2000,4000])
+# --weak
 axs.set_xticks([4,16,64,128,200,400,600,1200])
-axs.set_xticks(minor_ticks, minor=True)
 axs.set_yticks([4,16,64,128,200,400,600,1200])
-axs.set_yticks(minor_ticks, minor=True)
+minor_ticks = np.arange(3, 1200, 10)
+
+# -- strong
+#axs.set_xticks([64,200,400,600,1200])
+#axs.set_yticks([64,200,400,600,1200])
+#minor_ticks = np.arange(64, 1200, 10)
+
+
+
+axs.set_xticks(minor_ticks, minor=True)
 
 axs.get_xaxis().set_major_formatter(matplotlib.ticker.ScalarFormatter())
 axs.get_yaxis().set_major_formatter(matplotlib.ticker.ScalarFormatter())
-#plt.text(1000,1450, 'Ideal', fontsize=32,
-plt.text(800,800, 'Ideal', fontsize=32,
+plt.text(600,600, 'Ideal', fontsize=32,
                rotation=45, rotation_mode='anchor',color='maroon',alpha=0.6)
 plt.minorticks_on()
 plt.grid(which='major', linestyle='-')
 
 #plt.show()
-plt.savefig('speedup_per_epoch.png')
+plt.savefig('speedup_per_epoch_weak.png')
