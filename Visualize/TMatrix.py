d90e8ccb77544a2a91f64b1328acd3d53ce7a86a
ico1036 <jiwoong.kim@cern.ch>
2021-07-14 09:10:26 +0900

    2d distribution

--- Visualize/TMatrix.py
+++ Visualize/TMatrix.py
@@ -3,19 +3,20 @@ from ROOT import TMatrix
 import h5py 
 import numpy as np 
 
-infile_QCD = "QCD_HT1000/QCD_HT1000_0.h5"
 #infile_RPV = "RPV/RPV_0.h5"
 #dat = h5py.File(infile_RPV)
+
+
+infile_QCD = "QCD_HT1000/QCD_HT1000_0.h5"
 dat = h5py.File(infile_QCD)
 
-
-#target = 'ECAL'
+target = 'ECAL'
 #target = 'HCAL'
-target = 'TRACK'
+#target = 'TRACK'
 
 
 data = dat['all_events'][target][0]
-print(data)
+print(data.max)
 
 
 
@@ -36,24 +37,40 @@ gStyle.SetTitleFontSize(25)
 h2 = TH2F(mat)
 
 h2.GetXaxis().SetNdivisions(8)
-h2.GetXaxis().SetTitle('#eta pixel')
+h2.GetXaxis().SetTitle('#phi pixel')
+h2.GetXaxis().ChangeLabel(1,-1,-1,-1,-1,-1,"-3.15")
+h2.GetXaxis().ChangeLabel(2,-1,-1,-1,-1,-1,"-1.07")
+h2.GetXaxis().ChangeLabel(3,-1,-1,-1,-1,-1,"0.169")
+h2.GetXaxis().ChangeLabel(4,-1,-1,-1,-1,-1,"1.41")
+h2.GetXaxis().ChangeLabel(5,-1,-1,-1,-1,-1,"2.82")
+
 h2.GetXaxis().SetTitleOffset(1.5)
-h2.GetXaxis().SetTitleSize(0.05)
-h2.GetXaxis().SetLabelSize(0.04)
+h2.GetXaxis().SetTitleSize(0.06)
+h2.GetXaxis().SetLabelSize(0.05)
 
 
 h2.GetYaxis().SetNdivisions(8)
-h2.GetYaxis().SetTitle('#phi pixel')
+h2.GetYaxis().SetTitle('#eta pixel')
+
+h2.GetYaxis().ChangeLabel(1,-1,-1,-1,-1,-1,"-2.5")
+h2.GetYaxis().ChangeLabel(2,-1,-1,-1,-1,-1,"-0.848")
+h2.GetYaxis().ChangeLabel(3,-1,-1,-1,-1,-1,"0.134")
+h2.GetYaxis().ChangeLabel(4,-1,-1,-1,-1,-1,"1.12")
+h2.GetYaxis().ChangeLabel(5,-1,-1,-1,-1,-1,"2.23")
+
+
+
 h2.GetYaxis().SetTitleOffset(1.5)
-h2.GetYaxis().SetTitleSize(0.05)
-h2.GetYaxis().SetLabelSize(0.04)
+h2.GetYaxis().SetTitleSize(0.06)
+h2.GetYaxis().SetLabelSize(0.05)
 
 
-#h2.GetZaxis().SetTitle('Energy density [GeV]')
-h2.GetZaxis().SetTitle('p_{T} density [GeV]')
+h2.GetZaxis().SetNdivisions(8)
+h2.GetZaxis().SetTitle('Energy density [GeV]')
+##h2.GetZaxis().SetTitle('p_{T} density [GeV]')
 #h2.GetZaxis().SetTitleOffset(1.5)
 h2.GetZaxis().SetTitleSize(0.05)
-h2.GetZaxis().SetLabelSize(0.04)
+h2.GetZaxis().SetLabelSize(0.05)
 
 
 
@@ -61,6 +78,7 @@ outname = target + '.png'
 
 c1 = TCanvas("c1","c2",600,400)
 c1.Update()
+
 #h2.Draw('LEGO2Z')
 h2.Draw("LEGO2 0")
 c1.SetGrid()
